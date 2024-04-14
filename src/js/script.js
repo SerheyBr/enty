2326035b0ded752adbe9f4741e8b3694e164212d
Searhey <sbrilkov111@gmail.com>
2024-04-14 17:29:47 +0300

    add section main

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,13 @@
-document.querySelector(".burger-menu").addEventListener("click", function () {
-  this.classList.toggle("open");
+const burger = document.querySelector(".burger-menu");
+
+burger.addEventListener("click", function (e) {
+  const body = document.body;
+
+  e.currentTarget.classList.toggle("open");
+
+  if (burger.classList.contains("open")) {
+    body.style.overflow = "hidden";
+  } else {
+    body.style.overflow = "visible";
+  }
 });
