749e6f0195656448b534fb8e666f6f0f70fac13b
SerheyBr <sbrilkov111@gmail.com>
2024-04-16 22:48:43 +0300

    add section features

--- src/js/script.js
+++ src/js/script.js
@@ -11,3 +11,35 @@ burger.addEventListener("click", function (e) {
     body.style.overflow = "visible";
   }
 });
+
+const radioBtns = document.querySelectorAll('input[name = "radio"]');
+const imgRadio = document.querySelector(
+  ".features-unlimited-invoicing__img > img"
+);
+
+radioBtns.forEach((el) => {
+  el.addEventListener("click", (e) => {
+    switch (e.target.value) {
+      case "1": {
+        imgRadio.src =
+          "http://127.0.0.1:5500/src/images/features/img-unlimted1.svg";
+        break;
+      }
+      case "2": {
+        imgRadio.src =
+          "http://127.0.0.1:5500/src/images/features/img-unlimted2.svg";
+        break;
+      }
+      case "3": {
+        imgRadio.src =
+          "http://127.0.0.1:5500/src/images/features/img-unlimted3.svg";
+        break;
+      }
+      case "4": {
+        imgRadio.src =
+          "http://127.0.0.1:5500/src/images/features/img-unlimted4.svg";
+        break;
+      }
+    }
+  });
+});
