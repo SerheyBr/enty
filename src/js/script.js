ab3d29aa7ce5ce3cdbc3c7f3c60cfc3097b1505c
SerheyBr <sergeydrilkov@mail.ru>
2024-04-20 22:42:27 +0300

    add "rad-more", "subscribe", "simplify", "foter" sections

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
+import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";
+
 const burger = document.querySelector(".burger-menu");
 
 burger.addEventListener("click", function (e) {
@@ -45,3 +47,25 @@ radioBtns.forEach((el) => {
 });
 
 //проверка
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "vertical",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
