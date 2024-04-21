ff7055d21274c7496e29baa208412332e23d4646
SerheyBr <sergeydrilkov@mail.ru>
2024-04-21 22:43:26 +0300

    add scrolling animation

--- src/js/script.js
+++ src/js/script.js
@@ -103,3 +103,18 @@ const swiperTrust = new Swiper(".trustSwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+// add scrolling animation
+function onEntry(entry) {
+  entry.forEach((change) => {
+    if (change.isIntersecting) {
+      change.target.classList.add("element-show");
+    }
+  });
+}
+let options = { threshold: [0.1] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll(".element-animation");
+for (let elm of elements) {
+  observer.observe(elm);
+}
